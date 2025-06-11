0ebf881a6f232552c2a0fcf59e22125974c35c92
User <yonis676@gmail.com>
2025-06-11 23:24:50 +0300

    feat(shared): add shared utility functions and use them in host and remote apps

--- node_modules/host/src/App.tsx
+++ node_modules/host/src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 const RemoteHelloWorld = React.lazy(() => import("remote/HelloWorld"));
 import { APP_NAME } from "shared/src/constants";
 import type {User} from "shared/src/types";
+import { capitalize, formatEmail } from "shared/src/utils";
 
 const mockUser: User = {
     id: "1",
@@ -13,8 +14,11 @@ const mockUser: User = {
 export default function App() {
     return (
         <div className="App">
-            return <div>Welcome, {mockUser.name}!</div>;
-            <div>SHARED CONSTANTS:{APP_NAME}</div>
+            <div>
+                Welcome, {capitalize(mockUser.name)}!
+                <br />
+                Email: {formatEmail(mockUser.email)}
+            </div>            <div>SHARED CONSTANTS:{APP_NAME}</div>
             <h1>Host App</h1>
             <Suspense fallback={<div>Loading Remote…</div>}>
                 <RemoteHelloWorld />
--- node_modules/remote/src/App.tsx
+++ node_modules/remote/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { APP_NAME } from "shared/src/constants";
 import type {User} from "shared/src/types";
+import { capitalize, formatEmail } from "shared/src/utils";
 
 const mockUser: User = {
     id: "1",
@@ -13,8 +14,11 @@ const SharedButton = React.lazy(() => import("host/SharedButton"));
 function App() {
     return (
         <div>
-            return <div>Welcome, {mockUser.name}!</div>;
-
+            <div>
+                Welcome, {capitalize(mockUser.name)}!
+                <br />
+                Email: {formatEmail(mockUser.email)}
+            </div>
             <div>SHARED CONSTANTS:{APP_NAME}</div>
             <h1>Remote App</h1>
             <Suspense fallback={<span>Loading shared button…</span>}>
