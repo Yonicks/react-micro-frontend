b8c073f21de7ae2ffae8c1c01e2bf62f7df2a25a
User <yonis676@gmail.com>
2025-06-11 23:21:10 +0300

    feat(shared): add shared TypeScript types and use User type in host and remote

--- node_modules/host/src/App.tsx
+++ node_modules/host/src/App.tsx
@@ -1,11 +1,19 @@
 import React, { Suspense } from "react";
 const RemoteHelloWorld = React.lazy(() => import("remote/HelloWorld"));
 import { APP_NAME } from "shared/src/constants";
+import type {User} from "shared/src/types";
+
+const mockUser: User = {
+    id: "1",
+    name: "Yoni",
+    email: "yoni@example.com",
+};
 
 
 export default function App() {
     return (
         <div className="App">
+            return <div>Welcome, {mockUser.name}!</div>;
             <div>SHARED CONSTANTS:{APP_NAME}</div>
             <h1>Host App</h1>
             <Suspense fallback={<div>Loading Remote…</div>}>
--- node_modules/remote/src/App.tsx
+++ node_modules/remote/src/App.tsx
@@ -1,12 +1,20 @@
 import React, { Suspense } from "react";
 import { APP_NAME } from "shared/src/constants";
+import type {User} from "shared/src/types";
 
+const mockUser: User = {
+    id: "1",
+    name: "Yoni",
+    email: "yoni@example.com",
+};
 // Import the button from host, federated!
 const SharedButton = React.lazy(() => import("host/SharedButton"));
 
 function App() {
     return (
         <div>
+            return <div>Welcome, {mockUser.name}!</div>;
+
             <div>SHARED CONSTANTS:{APP_NAME}</div>
             <h1>Remote App</h1>
             <Suspense fallback={<span>Loading shared button…</span>}>
